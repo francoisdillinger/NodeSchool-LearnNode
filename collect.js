5517862806d7bf8037d30d3eac771d2d2c159f52
James Jester <elmojester@gmail.com>
2016-08-11 11:25:20 -0500

    erasing old code.

--- collect.js
+++ collect.js
@@ -21,16 +21,5 @@ http.get(path, function(response){
 })
 
 
-// var http = require('http')  
-//      var bl = require('bl')  
-       
-//      http.get(process.argv[2], function (response) {  
-//        response.pipe(bl(function (err, data) {  
-//          if (err)  
-//            return console.error(err)  
-//          data = data.toString()  
-//          console.log(data.length)  
-//          console.log(data)  
-//        }))    
-//      })  
+
    
